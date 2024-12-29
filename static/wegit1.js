bfba12706fe4b7e27447b5c2fadadf700e8eeeee
Jacell <160160322+JACELL100@users.noreply.github.com>
2024-12-29 16:28:58 +0530

    Add files via upload

---
+++ static/wegit1.js
@@ -0,0 +1,15 @@
+{
+  /* <script type="text/javascript">
+    (function(w, d, s, i) {
+        var script = d.createElement(s);
+        script.async = true;
+        script.src = "//widget.simplybook.me/v2/widget/widget.js";
+        script.onload = function() {
+            new SimplybookWidget({"widget_type":"button","url":"https:\/\/ettarracoffeesupperclub.simplybook.me","theme":"minimal","theme_settings":{"timeline_modern_display":"as_slots","hide_company_label":"0","timeline_hide_unavailable":"1","hide_past_days":"0","timeline_show_end_time":"1","sb_base_color":"#cb8d75","btn_color_1":"#f8ad31,#fc591e,#fc591e","link_color":"#cb8d75","display_item_mode":"block","body_bg_color":"#ffffff","sb_review_image":"","dark_font_color":"#403733","light_font_color":"#ffffff","sb_company_label_color":"#ffffff","hide_img_mode":"0","sb_busy":"#c7b3b3","sb_available":"#2b212b"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]},"button_title":"Book now","button_background_color":"#cb8d75","button_text_color":"#ffffff","button_position":"right","button_position_offset":"55%","container_id":"sbw_51ayxu"});
+        };
+        d.head.appendChild(script);
+    })(window, document, 'script', 'sbw_51ayxu');
+</script> */
+}
+
+//<div id="sbw_51ayxu"></div>//
